54e857ce7cf9aed85e23763d96ded6723a10ecd3
Laura Aguilera Checa <lac743@inlumine.ual.es>
2019-05-10 16:57:26 +0200

    Tildes y cosas de esas

--- instagual/src/main/java/user_interface/Privacidad.java
+++ instagual/src/main/java/user_interface/Privacidad.java
@@ -2,4 +2,14 @@ package user_interface;
 
 public class Privacidad {
 	public Ajustes _ajustes;
+	private event _cuenta_privada;
+	private event _mostrar_seguidores;
+	
+	public void Cuenta_privada() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void Mostrar_seguidores() {
+		throw new UnsupportedOperationException();
+	}
 }
--- instagual/src/main/java/user_interface/Publicacion_busqueda_inv_.java
+++ instagual/src/main/java/user_interface/Publicacion_busqueda_inv_.java
@@ -2,5 +2,5 @@ package user_interface;
 
 public class Publicacion_busqueda_inv_ {
 	public Listado_de_publicaciones_inv_ _listado_de_publicaciones_inv_;
-	public Ver_publicación_inv_ _ver_publicación_inv_;
+	public Ver_publicacion_inv_ _ver_publicacion_inv_;
 }
