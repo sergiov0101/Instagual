7cf546615defa95611b6de5f2cfa38f1b8e19f46
SERGIO VERA OCHOA <svo585@inlumine.ual.es>
2019-06-04 12:47:25 +0200

    listado de usuarios

--- instagual/src/main/java/user_interface/Iniciar_sesion.java
+++ instagual/src/main/java/user_interface/Iniciar_sesion.java
@@ -6,10 +6,12 @@ import user_interface.*;
 
 public class Iniciar_sesion extends Iniciar_sesion_ventana {
 	Buscar__inv_ buscarInvitado = new Buscar__inv_();
+	Listado_de_publicaciones_populares_inv_ listadoPublicaciones = new Listado_de_publicaciones_populares_inv_();
 	public Usuario_Sin_Registrar _usuario_Sin_Registrar;
 	public Iniciar_sesion() {
-		mainLayout.addComponent(buscarInvitado);
+		mainLayout.addComponent(listadoPublicaciones);
 		buscarInvitado.setVisible(false);
+		listadoPublicaciones.setVisible(false);
 		buttonIniciarSesionInv.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
 				
@@ -26,7 +28,8 @@ public class Iniciar_sesion extends Iniciar_sesion_ventana {
 		loginVertLayout.setVisible(false);
 		horizontalBotones.setVisible(false);
 		mainLayout.removeStyleName("LoginPricipalLayout");
-		buscarInvitado.setVisible(true);
+		buscarInvitado.setVisible(false);
+		listadoPublicaciones.setVisible(true);
 		
 		//buttonIniciarSesion.addComponent();
 	}
--- instagual/src/main/java/user_interface/Publicacion.java
+++ instagual/src/main/java/user_interface/Publicacion.java
@@ -1,7 +1,11 @@
 package user_interface;
-
-public class Publicacion extends Acciones_de_publicacion_inv_ {
+import com.instagual.instagual.publicacion_ventana;
+public class Publicacion extends publicacion_ventana {
 	public Ver_listado_de_publicaciones _ver_listado_de_publicaciones;
 	public Ver_publicacion _ver_publicacion;
 	public Acciones_publicacion _acciones_publicacion;
+	public Publicacion() {
+		
+	}
+	
 }
